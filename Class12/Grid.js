9a107f0a1d1a9155ce45ff9ce0a2ac433962c47d
Buta <belong6928@naver.com>
2022-12-06 16:00:38 +0900

    2048 미완

--- Class12/Grid.js
+++ Class12/Grid.js
@@ -12,9 +12,15 @@
 let bg_block = document.getElementsByClassName("bg_block");
 let cellLength = bg_block.length;
 let board = new Array(4);
+let scoreDiv = document.getElementById("score");
+let btn = document.getElementById("btn");
+let score = 0;
 
 let overCheck = 1;
 let numCheck = 1;
+let gameState = 0;
+
+var cnt = Math.floor(Math.random() * 2.99);
 
 for(let i=0; i<4; i++){
     board[i] = new Array(4);
@@ -28,6 +34,29 @@ for(let i=0; i<4; i++) {
     }
 }
 
+btn.addEventListener("click", function(){
+    gameState = 1;
+    if(gameState == 1){
+        window.addEventListener("keydown", (e)=> {
+            const keyCode = e.keyCode;
+            if(keyCode == 37) {
+                moveLeft();
+            }
+            if(keyCode == 38) {
+                moveUp();
+            }
+            if(keyCode == 39) {
+                moveRight();
+            }
+            if(keyCode == 40) {
+                moveDown();
+            }else if(keyCode == 78){
+                randomNum();
+            }
+        });
+    }
+});
+
 init();
 //초기화 함수
 function init(){
@@ -36,8 +65,16 @@ function init(){
             board[i][j] = 0;
         }
     }
-    randomNum();
-    randomNum();
+    if(cnt == 0){
+        randomNum();
+    }
+    if(cnt == 1){
+        randomNum1();
+    }
+    if(cnt == 2){
+        randomNum();
+        randomNum1();
+    }
     update();
 }
 
@@ -47,26 +84,43 @@ function update(){
         for(let j=0; j<4; j++){
             bg_block[count].innerHTML = board[i][j] == 0 ? "" : board[i][j];
             count++;
+            scoreDiv.innerHTML = "Score : " + score;
         }
     }
 }
 
 function randomNum(){
-    ranPlaceX = Math.floor(Math.random() * 3.99); 
-    ranPlaceY = Math.floor(Math.random() * 3.99);
+    ranPlaceX = parseInt(Math.random() * 3.99); 
+    ranPlaceY = parseInt(Math.random() * 3.99);
+    var create = Math.floor(Math.random() * 2.99);
+
     if(board[ranPlaceX][ranPlaceY] == 0) {
-        board[ranPlaceX][ranPlaceY] = 2;
-    }
-    else {
+        if(create == 0){
+            board[ranPlaceX][ranPlaceY] = 2;
+        }
+        else {
         randomNum();
+        }
+        //update();
     }
-    update();
 }
 
+function randomNum1(){
+    ranPlaceX = parseInt(Math.random() * 3.99); 
+    ranPlaceY = parseInt(Math.random() * 3.99);
+    var create = Math.floor(Math.random() * 2.99);
+
+    if(board[ranPlaceX][ranPlaceY] == 0) {
+        board[ranPlaceX][ranPlaceY] = 4;
+
+    } else {
+        randomNum1();
+    }
+    //update();
+}
 
 function moveLeftNum() {
     let k;
-
     numCheck = 1
 
     for(let i = 0; i < 4; i++) {
@@ -79,7 +133,6 @@ function moveLeftNum() {
                     }
                     board[i][k-1] = board[i][k];
                     board[i][k] = 0;
-                    
                     k--;
                     numCheck = 0;
                 }
@@ -89,8 +142,9 @@ function moveLeftNum() {
 }
 
 function moveLeft() {
+    gameOver();
     moveLeftNum();
-
+    score++;
     for(let i = 0; i < 4; i++) {
         for(let j = 1; j < 4; j++) {
             if(board[i][j] == board[i][j+1] && board[i][j] != 0) {
@@ -136,8 +190,9 @@ function moveUpNum() {
 }
 
 function moveUp() {
+    gameOver();
     moveUpNum();
-
+    score++;
     for(let i = 0; i < 4; i++) {
         for(let j = 1; j < 3; j++) {
             if(board[j][i] == board[j+1][i] && board[j][i] != 0) {
@@ -179,8 +234,9 @@ function moveRightNum() {
 }
 
 function moveRight() {
+    gameOver();
     moveRightNum();
-
+    score++;
     for(let i = 0; i < 4; i++) {
         for(let j = 2; j > -1; j--) {
             if(board[i][j] == board[i][j-1] && board[i][j] != 0) {
@@ -205,7 +261,6 @@ function moveDownNum() {
 
     for(let i = 0; i < 4; i++) {
         for(let j = 2; j > -1; j--) {
-            console.log(board[i][j]);
             if(board[j][i] != 0) {
                 k = j;
                 while(1) {
@@ -226,8 +281,9 @@ function moveDownNum() {
 }
 
 function moveDown(){
+    gameOver();
     moveDownNum();
-
+    score++;
     for(let i = 0; i < 4; i++) {
         for(let j = 3; j > 0; j--) {
             if(board[j][i] == board[j-1][i] && board[j][i] != 0) {
@@ -245,40 +301,42 @@ function moveDown(){
     }
 }
 
-// function rowCheck() {
-//     for(let i = 1; i < 5; i++) {
-//         for(let j = 1; j < 4; j++) {
-//             if(board[i][j] == board[i][j+1]) {
-//                 overCheck = 0;
-//             }
-//         }
-//     }
-// }
+function rowCheck() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 3; j++) {
+            if(board[i][j] == board[i][j+1]) {
+                overCheck = 0;
+            }
+        }
+    }
+}
 
-// function columnCheck() {
-//     for(let i = 1; i < 5; i++) {
-//         for(let j = 1; j < 4; j++) {
-//             if(board[j][i] == board[j+1][i]) {
-//                 overCheck = 0;
-//             }
-//         }
-//     }
-// }
+function columnCheck() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 3; j++) {
+            if(board[j][i] == board[j+1][i]) {
+                overCheck = 0;
+            }
+        }
+    }
+}
 
-window.addEventListener("keydown", (e)=> {
-    const keyCode = e.keyCode;
-    if(keyCode == 37) {
-        moveLeft();
+function gameOver() { //게임오버
+    let fullCheck = 1;
+
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            if(board[i][j] == 0) {
+                fullCheck = 0;
+            }
+        }
     }
-    else if(keyCode == 38) {
-        moveUp();
+    rowCheck();
+    columnCheck();
+    if(fullCheck && overCheck) {
+        alert("Gameover\n" + "Score : " + score);
+        window.location.reload();
     }
-    else if(keyCode == 39) {
-        moveRight();
-    }
-    else if(keyCode == 40) {
-        moveDown();
-    }else if(keyCode == 78){
-        randomNum();
-    }
-});
+
+    overCheck = 1;
+}
